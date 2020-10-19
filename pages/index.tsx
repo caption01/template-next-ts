2737ce477c2e201de9fdd9c3f2bdf7d37b536ef7
nutchapon.h <nutchapon@enres.com>
2020-10-19 21:00:24 +0700

    #2 setup antd lib

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import React, { FunctionComponent } from 'react'
+import { Button } from 'antd'
 
 const MainPage: FunctionComponent = () => {
-  return <div>Hello Typescript</div>
+  return (
+    <div>
+      Hello Typescript
+      <Button type="primary" shape="round">Primary Button</Button>
+    </div>
+  )
 }
 
 export default MainPage
