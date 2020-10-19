6ef7613c2f8bdb7e28a2856da72c1a308aafd401
nutchapon.h <nutchapon@enres.com>
2020-10-19 21:07:05 +0700

    #3 setup styled-component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import React, { FunctionComponent } from 'react'
 import { Button } from 'antd'
+import styled from 'styled-components'
+
+const StyledBtn = styled(Button)`
+  margin: 30px 30px;
+`
 
 const MainPage: FunctionComponent = () => {
   return (
     <div>
       Hello Typescript
-      <Button type="primary" shape="round">Primary Button</Button>
+      <StyledBtn type="primary" shape="round">Primary Button</StyledBtn>
     </div>
   )
 }
